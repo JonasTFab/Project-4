431c30ec3f3b1d0e7408d740a436b0d5ab4bcb28
George Cowie <gecocowie@live.com>
2019-11-01 12:27:54 +0100

    sweet home alabama

--- Phase_transitions/Phase_transitions_in_magnetic_systems/main.cpp
+++ Phase_transitions/Phase_transitions_in_magnetic_systems/main.cpp
@@ -4,8 +4,16 @@
 #include <armadillo>
 #include <cstdlib>
 #include <random>
+// compile with: g++ main.cpp -o main1 -larmadillo -llapack -lblas
+//mpicxx  -o main_mpi.x  main.cpp -std=c++11
+//mpiexec -n 2 ./main_mpi.x 8
 double J = 1;
 std::mt19937 generator (time(NULL)); //seed rng with time now
+
+// inline function for periodic boundary conditions
+inline int periodic(int i, int limit, int add) {
+return (i+limit+add) % (limit);
+}
 
 int spin(){ //generate random spins up or down with mersenne twister
   std::uniform_real_distribution<double> dis(0.0, 1.0);
@@ -21,9 +29,7 @@ int spin(){ //generate random spins up or down with mersenne twister
       }
 } // end of function ran()
 
-//we want a sample mean
-//sample eror
-double system(int L){
+arma::Mat<double> spin_system(int L){ //set up the lattice of spins
   arma::Mat<double> spin_matrix = arma::mat(L, L);
   for (int i = 0; i < L; i++){
     for(int j = 0; j < L; j++){
@@ -31,18 +37,18 @@ double system(int L){
     }
   }
   std::cout << spin_matrix << "\n";
-  return 0;
+  return spin_matrix;
 }
 
 int ising_model(int L, arma::mat spin_matrix){
   double energy = 0;
   for (int i = 0; i < L; i++){
     for(int j = 0; j < L; j++){
-    energy += spin_matrix(i,j)*(spin_matrix(i-1,j)+spin_matrix(i,j-1)+spin_matrix(i+1,j)+spin_matrix(i,j+1));
-
+    energy -= spin_matrix(i,j)*(spin_matrix(periodic(i,L,-1),j) + spin_matrix(i,periodic(j,L,-1))); //*(spin_matrix(i-1,j)+spin_matrix(i,j-1)+spin_matrix(i+1,j)+spin_matrix(i,j+1));//J = 1
+    std::cout << energy << std::endl;
     }
   }
-  energy *= -J;
+  //std::cout << energy << std::endl;
   return 0;
 }
 
@@ -51,7 +57,8 @@ int main(int argc, char* argv[]){
   int L;
   std::cout << "chose lattice length L. N = LXL: L= ";
   std::cin >> L;
-  system(L);
+  arma::Mat<double> matrix = spin_system(L);
+  ising_model(L,matrix);
   }
 
 
