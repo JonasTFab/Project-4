c1e39d115c55c8e6e29b0aa989426ee185712d67
George Cowie <gecocowie@live.com>
2019-11-15 12:19:47 +0100

    ferdi

--- Phase_transitions/Phase_transitions_in_magnetic_systems/plots.py
+++ Phase_transitions/Phase_transitions_in_magnetic_systems/plots.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 def c_plots(random_file,ordered_file):
+    num_particles = 20*20
     #split data random matrix in files into seperate types
     data_random_matrix = np.loadtxt(random_file, skiprows=1)
     data_ordered_matrix = np.loadtxt(ordered_file, skiprows=1)
@@ -70,7 +71,7 @@ def c_plots(random_file,ordered_file):
 #c_plots("MC_cycles_random_T1.txt","MC_cycles_ordered_T1.txt")
 
 def d_plots(filename):
-    plt.figure()
+    plt.figure(6)
     counted_energies = np.loadtxt(filename)
     mean = counted_energies[0]
     variance = counted_energies[1]
@@ -98,6 +99,7 @@ def d_plots(filename):
     plt.legend()
     plt.ylabel('P')
     plt.xlabel('Energy')
+    plt.show()
     "Remember to write that the expectation value calculated in main is at the top of this"
 
 #average energy for this system -499.874
@@ -116,50 +118,73 @@ def e_plots(filename,L,color):
     dat = np.transpose(np.loadtxt(filename))
     Temp,avg_e,h_capacity,avg_m,susceptibility = dat[0],dat[1],dat[2],dat[3],dat[4]
     #smoothed_h_capacity = pd.rolling_mean(h_capacity,30) #take a moving average over 30 measurments
-    smoothed_h_capacity = pd.Series(h_capacity).rolling(window=5).mean()
+    smoothed_h_capacity = pd.Series(h_capacity).rolling(window=20,center = True).mean()
+    smoothed_susceptibility = pd.Series(susceptibility).rolling(window=20, center = True).mean()
     idx = np.argmax(smoothed_h_capacity)
+    idx1 = np.argmax(smoothed_susceptibility)
     critical_T = Temp[idx]
-    Lattice_size = float(L)
-    T_C_infty = critical_T-a*Lattice_size**(-1/nu)
-    print(T_C_infty)
+    print(Temp[idx],'hcap',L)
+    print(Temp[idx1],'sus',L)
 
     plt.figure(1)
     ax1 = plt.subplot(211)
-    plt.plot(Temp,avg_e/particles,color)
-    plt.title("Average energy and magnetization")
+    ax1.plot(Temp,avg_e/particles,color)
+    plt.title("Average energy and magnetization",fontsize=15)
     box = ax1.get_position()
     ax1.set_position([box.x0, box.y0+box.height*0.04, box.width, box.height])
     plt.grid()
-    plt.ylabel("E")
+    plt.ylabel("E",fontsize=15)
     ax2 = plt.subplot(212)
     line = ax2.plot(Temp,avg_m/particles,color,label= "L=%s"%L)#,label =("Average magnetization"))
-    plt.xlabel("T")
-    plt.ylabel("M")
+    plt.xlabel("T",fontsize=15)
+    plt.ylabel("M",fontsize=15)
     plt.grid()
     #plt.show()
     box = ax2.get_position()
     ax2.set_position([box.x0, box.y0+box.height*0.04, box.width, box.height])
 
     # Put a legend to the right of the current axis
-    ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),ncol = 5,fancybox=True)
+    ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),ncol = 5,fancybox=True,fontsize=15)
 
 
     plt.figure(2)
     ax3 = plt.subplot(211)
-    ax3.plot(Temp,h_capacity,'.',color = color)#,label = ("Heat capacity"))
-    plt.plot(Temp,smoothed_h_capacity,color = color)
-    plt.ylabel("C_v")
+    plt.title("Heat capacity and susceptibility",fontsize=15)
+    ax3.plot(Temp,h_capacity/particles,color = color,alpha=0.4)#,label = ("Heat capacity"))
+    plt.plot(Temp,smoothed_h_capacity/particles,color = color)
+    plt.ylabel("$C_v$",fontsize=15)
     plt.grid()
     ax4 = plt.subplot(212)
-    plt.plot(Temp,susceptibility,color)#,label = ("Susceptibility"))
-    plt.xlabel("T")
-    plt.ylabel("\u03A7")
+    ax4.plot(Temp,susceptibility/particles,color = color,alpha = 0.4)#,label = ("Susceptibility"))
+    ax4.plot(Temp,smoothed_susceptibility/particles,color = color,label= "L=%s"%L)
+    plt.xlabel("T",fontsize=15)
+    plt.ylabel("$\chi$",fontsize=15)#("\u03A7")
     plt.grid()
+    box = ax4.get_position()
+    ax4.set_position([box.x0, box.y0+box.height*0.04, box.width, box.height])
+    # Put a legend to the right of the current axis
+    ax4.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),ncol = 5,fancybox=True,fontsize=15)
+    return(critical_T)
 
 
+T_40 = e_plots("4e_L40.txt","40","r")
+T_60 = e_plots("4e_L60.txt","60","b")
+T_80 = e_plots("4e_L80.txt","80","g")
+T_100 = e_plots("4e_L100.txt","100","black")
 
-e_plots("4e_L40.txt","40","r")
-e_plots("4e_L60.txt","60","b")
-e_plots("4e_L80.txt","80","g")
-e_plots("4e_L100.txt","100","black")
+plt.show()
+
+nu = 1.
+L_size = np.array([40.,60.,80.,100.])
+Tc_vals = np.array([T_40,T_60,T_80,T_100])
+
+lin_fit = np.polyfit(Tc_vals/L_size,Tc_vals,1)
+a = lin_fit[0]
+print(lin_fit)
+x = np.linspace(-0.01,0.055,100)
+critical_T = a*L_size**(-nu)+Tc_vals
+print(critical_T)
+plt.plot(Tc_vals/L_size,Tc_vals,"o",color = "r")
+plt.plot(x,lin_fit[0]*x +lin_fit[1])
+plt.grid()
 plt.show()
